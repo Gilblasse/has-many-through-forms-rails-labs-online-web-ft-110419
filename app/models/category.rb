e614be9871f9b72d3b127aa54c7eeeed3ee59d75
Gilblasse <gilblasse@gmail.com>
2020-01-30 12:55:59 -0500

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,11 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
+
+
+
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  end
+
 end
--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,4 +1,16 @@
 class PostCategory < ActiveRecord::Base
   belongs_to :post
   belongs_to :category
+  
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
+
+
 end
